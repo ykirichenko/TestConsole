8d598c8da569166f18f19c0b6193ed6fd2d435ae
ykirichenko <k.yurets@gmail.com>
2014-10-23 19:37:35 +0300

    some changes to branch 1.1

--- TestGitConsoleApplication/Program.cs
+++ TestGitConsoleApplication/Program.cs
@@ -12,6 +12,7 @@ namespace TestGitConsoleApplication
         {
             Console.WriteLine("The first change");
 			// some changes to branch 1
+			// some changes to branch 1.1
         }
     }
 }
